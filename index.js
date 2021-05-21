82c36c731d17d8c67bbad3119d9a4c23bb10735a
Ivan Candido <ivan.candido@powerhrg.com>
2021-05-21 14:28:24 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    let distance;
+    let hq = 42;
+    if (hq > blocks){
+        distance = hq - blocks;
+    } else {
+        distance = blocks - hq;
+    }
+    return distance;
+}
+
+function distanceFromHqInFeet(feet){
+    let blockToFeet = 264;
+    return distanceFromHqInBlocks(feet) * blockToFeet;
+    
+}
+function distanceTravelledInFeet(start, end){
+    if (start > end){
+        return (start - end) * 264;
+    } else {
+        return (end - start) * 264;
+    }
+}
+function calculatesFarePrice(start, end){
+    let feet = distanceTravelledInFeet(start, end);
+    if (feet <= 400){
+        return 0;
+    } else if (feet > 400 && feet <= 2000){
+        return (feet - 400) * .02;
+    } else if (feet > 2000 && feet <= 2500){
+        return 25;
+    } else if (feet > 2500){
+        return 'cannot travel that far';
+    }
+}
